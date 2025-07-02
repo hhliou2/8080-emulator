ff366916fc1fa0a7cff6ef95453fb15dfc04fb1f
hhliou2 <liouhasan@gmail.com>
2025-07-02 16:09:52 -0700

    seperated struct declarations to header file for future sharing

--- Emulate8080.c
+++ Emulate8080.c
@@ -1,34 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
+#include "Emulate8080.h"
 
-// Data structure for each condition code found in 8080
-typedef struct ConditionCodes {
-	uint8_t	z:1;
-	uint8_t	s:1;
-	uint8_t	p:1;
-	uint8_t	cy:1;
-	uint8_t	ac:1;
-	uint8_t	pad:3;
-} ConditionCodes;
-
-// Data structure for current memory state, including registers and RAM
-typedef struct State8080 {
-	uint8_t	a;
-	uint8_t	b;
-	uint8_t	c;
-	uint8_t	d;
-	uint8_t	e;
-	uint8_t	h;
-	uint8_t	l;
-	uint16_t	sp;
-	uint16_t	pc;
-	uint8_t	*memory;
-	struct	ConditionCodes cc;
-	uint8_t	int_enable;
-} State8080;
+#define DEBUG 0
 
 // Error call for opcode
 void UnimplementedInstruction(State8080* state) {
